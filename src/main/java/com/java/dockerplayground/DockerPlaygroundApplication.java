6c4c4b742164a715d4f2a47e4b81be2ee5423baa
Yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2023-12-25 20:45:50 +0530

    Added customised banner for application and added commands around image

--- src/main/java/com/java/dockerplayground/DockerPlaygroundApplication.java
+++ src/main/java/com/java/dockerplayground/DockerPlaygroundApplication.java
@@ -11,7 +11,6 @@ public class DockerPlaygroundApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world!!");
 		SpringApplication.run(DockerPlaygroundApplication.class, args);
 	}
 
